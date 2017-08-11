3122e52c7c90bdca7a03fbcfb4ac461ad0feafb5
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-08-11 14:29:25 +0800

    Lua 学习

--- 7_FunctionLua.lua
+++ 7_FunctionLua.lua
@@ -1,6 +1,6 @@
 
 
-
+--[[
 function fact(n)
 	if n==1 then
 		return n;
@@ -23,3 +23,26 @@ print(fact(10));
 
 
 --print(fact("ee"));
+]]--
+
+function TestFun(tab,fun)
+	for k,v in pairs(tab) do
+		fun(k,v);
+	end
+end
+
+function F1 (k,v)
+	print(k.."___________"..v);
+end
+
+
+function F2 (k,v)
+	print(k.."==========="..v);
+end
+
+tab={key1="val1",key2="val2"};
+TestFun(tab,F1);
+TestFun(tab,F2);
+TestFun(tab,function ( k,v)
+	print(k.."♂"..v);
+end);
---
+++ 8_LocalLua.lua
@@ -0,0 +1,31 @@
+a=5;
+
+print(type(a));
+
+a = a.."1";
+
+print(type(a));
+
+local b= 10;
+
+print (b);
+
+function  Test(  )
+	c=5;
+	local d = 6;
+end
+
+Test();
+
+print(c);
+
+print(d);
+
+
+do
+	local a=10
+	b=11
+	print(a,b);
+end
+
+print(a,b);
