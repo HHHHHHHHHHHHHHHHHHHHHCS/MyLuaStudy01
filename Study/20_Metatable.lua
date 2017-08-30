186e31cd19033a3ebb26594afc9bfd30e28a9774
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-08-30 17:33:07 +0800

    元表更新

--- Study/20_Metatable.lua
+++ Study/20_Metatable.lua
@@ -20,3 +20,33 @@ print(mymetaTable);
 
 newTable = setmetatable({"Lua","Java","C#","C++","Javascript"}
 	,{1,2,3,4,5,6,7,8,9,10});
+
+
+print("=========================");
+
+--   使用 __metatable   可以保护元表
+--  如果元表(metatable)中存在__metatable键值，setmetatable会失败 。
+tab= setmetatable({"Lua","Java","C#","C++","Javascript"},{__metatable="ABCDEFG"});
+print(getmetatable(tab));
+
+
+print("=========================");
+
+
+mymetaTable = 
+{
+	__index = 
+	{
+	[7]="Ruby",
+	[8]="Python",
+	function (tab,key)
+		print("访问不存在的索引的时候 调用我:",tab,key);
+	end
+}
+}
+
+tab= setmetatable({"Lua","Java","C#","C++","Javascript"},mymetaTable);
+
+print(tab[1]);
+print(tab[8]);
+print(tab[10]);
