538647ff6de2cd8148ef841b6fe1281284480876
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-10-03 23:54:27 +0800

    私有 和 _G

--- LuaBook/20_Private.lua
+++ LuaBook/20_Private.lua
@@ -34,3 +34,53 @@ acc1.widthdraw(40)
 print(acc1.getBalance())
 acc1.deposit(80)
 print(acc1.getBalance())
+
+
+print("=========================")
+
+function newObject(value)
+    return function (action,v)
+        if action=="get" then 
+            return value 
+        elseif action=="set" then
+            value=v
+        else
+            error("invalid action")
+        end
+    end
+end
+
+d=newObject(10)
+print(d("get"))
+d("set",20)
+print(d("get"))
+
+print("=========================")
+
+local balance={}
+
+Account={}
+
+function Account:widthdraw(v)
+    balance[self]=balance[self]-v
+end
+
+function Account:deposit(v)
+    balance[self]=balance[self]+v
+end
+
+function Account:balance()
+    return balance[self]
+end
+
+function Account:new(o)
+    o=o or {}
+    setmetatable(o, self)
+    self.__index=self
+    balance[o]=0
+    return o
+end
+
+a=Account:new{}
+a:deposit(100)
+print(a:balance())
---
+++ LuaBook/21_Environment.lua
@@ -0,0 +1,73 @@
+for n in pairs(_G) do
+    print(n)
+end
+
+print("=================")
+
+function getfield(f)
+    local v = _G
+    for w in string.gmatch(f, "[%a_][%w_]*") do
+        if v ~= nil and type(v) == "table" then
+            v = v[w]
+        else
+            return nil
+        end
+    end
+    return v
+end
+
+function setfield(f, v)
+    local t = _G
+    for w, d in string.gmatch(f, "([%a_][%w_]*)(%.?)") do
+        if d == "." then
+            t[w] = t[w] or {}
+            t = t[w]
+        else
+            t[w] = v
+        end
+    end
+end
+
+getfield("a.b.c.d")
+setfield("a.b.c.d.e", 10)
+print(getfield("a.b.c.d.e"))
+print("========================")
+
+local  function declare( name,initval )
+    rawset(_G,name,initval or false)
+end
+
+local function get( name,initval )
+    rawget(_G,initval)
+end
+
+local declaredNames={}
+
+setmetatable(
+    _G,
+    {
+        __newindex = function(t,n,v)
+            if not declaredNames[n] then
+                local w=debug.getinfo( 2,"S" ).what--2:栈层  S:仅返回 what:Lua
+                if w~="main" and w~="C" then
+                    error("attempt to write to undeclared variable "..n,2)
+                end
+                declaredNames[n]=true
+            end
+            rawset(t,n,v)
+        end,
+
+        __index = function(_, n)
+            if not declaredNames[n] then
+                error("attempt to read undeclared variable "..n,2)
+            else 
+                return nil
+            end
+        end
+    }
+)
+
+
+print(z)--error:
+mm={}--error:
+
