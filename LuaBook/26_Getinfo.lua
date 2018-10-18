028b0ecdc021f6ad89e2f4041296cd296d19bf0a
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-10-19 00:13:43 +0800

    局部获取变量 协程获取变量

--- LuaBook/26_Getinfo.lua
+++ LuaBook/26_Getinfo.lua
@@ -33,3 +33,70 @@ function foo(a, b)
 end
 
 foo(10, 20)
+
+print("==================================")
+function getvarvalue(name, level, isenv)
+    local value
+    local found = false
+
+    level = (level or 1) + 1
+
+    --先从局部变量开始找
+    for i = 1, math.huge do
+        local n, v = debug.getlocal(level, i)
+        if not n then
+            break
+        end
+        if n == name then
+            value = v
+            found = true
+        end
+    end
+    if found then
+        return "local", value
+    end
+
+    local func = debug.getinfo(level, "f").func
+    --尝试非局部变量
+    for i = 1, math.huge do
+        local n, v = debug.getupvalue(func, i)
+        if not n then
+            break
+        end
+        if n == name then
+            return "upvalue", v
+        end
+    end
+
+    if isenv then
+        return "noenv"
+    end
+
+    --没找到,从环境中获取
+    local _, env = getvarvalue("_ENV", level, true)
+    if env then
+        return "global", env[name]
+    else
+        return "noenv"
+    end
+end
+
+local a = 4
+print(getvarvalue("a"))
+tt = "xx"
+print(getvarvalue("tt"))
+print("==================================")
+
+co =
+    coroutine.create(
+    function()
+        local x = 10
+        coroutine.yield()
+        error("some error")
+    end
+)
+
+coroutine.resume(co)
+print(debug.traceback(co))
+print(coroutine.resume(co))
+print(debug.getlocal( co,1,1 ))
